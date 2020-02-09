229b6cf414a1f33ea9344007710b909c3b5fadba
Kresimir Vlahov <kresimir.vlahov@ericsson.com>
2020-02-09 17:52:35 +0100

    Finish frontend for sqlconnection objects

--- PPPK/PPPK/App_Start/BundleConfig.cs
+++ PPPK/PPPK/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace PPPK
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/moment.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-datepicker3.css"));
         }
     }
 }
