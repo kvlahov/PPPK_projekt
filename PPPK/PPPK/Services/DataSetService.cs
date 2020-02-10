3582d721a894c07de2e5cff45666278311b77dae
kvlahov <vlahov.kresimir@gmail.com>
2020-02-10 19:53:50 +0100

    Create DataSet service

---
+++ PPPK/PPPK/Services/DataSetService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PPPK.Services
+{
+    public class DataSetService
+    {
+        private SqlConnection _sqlConnection;
+
+    }
+}
